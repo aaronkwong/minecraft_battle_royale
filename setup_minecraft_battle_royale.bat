8ba9e2e698f12b05b9638f39bec41d7a1cf2aa2c
Aaron Wong <wongakk7@gmail.com>
2020-12-25 15:13:47 -0500

    added a bat file to make setup easier.

---
+++ setup_minecraft_battle_royale.bat
@@ -0,0 +1,6 @@
+curl --url https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar --output ./minecraft_server_1_15_2/server.jar
+python -m venv env
+CALL .\env\Scripts\activate
+python -m pip install --upgrade pip
+pip install -r ./env/requirements.txt
+pause
